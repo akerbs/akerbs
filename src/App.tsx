e87d0d0a658568de1e63d3c84b11fbcb111bbc9d
akerbs <akerbs@gmail.com>
2022-10-16 17:08:06 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
@@ -5,19 +6,23 @@ import "./css/App.css";
 import "./css/media.css";
 import { ContactPage } from './pages/ContactPage';
 import { HomePage } from './pages/HomePage';
-import { PortfolioPage } from './pages/PortfolioPage';
+// import { PortfolioPage } from './pages/PortfolioPage';
+
+const PortfolioPage = React.lazy(() => import('./pages/PortfolioPage'))
 
 
 function App() {
   return (
       <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/portfolio' element={<PortfolioPage />} />
-        <Route path='/contact' element={<ContactPage />} />
-      </Routes>
-      <Footer/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/portfolio' element={<PortfolioPage />} />
+            <Route path='/contact' element={<ContactPage />} />
+          </Routes>
+          <Footer/>
+      </Suspense>
       </div>
   );
 }
--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,13 +1,12 @@
 import { PortfolioItem } from '../components/PortfolioItem';
 import { items } from '../data/Items';
 
-export function PortfolioPage() {
+export default function PortfolioPage() {
 
   
   return (
     <div className="Portfolio-content">
       {items.map(item => <PortfolioItem item={item} key={item.id}/>)}
-
     </div>
   );
 }
