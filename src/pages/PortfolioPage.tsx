20484be804e42df87391f709df16eea260253e6a
akerbs <akerbs@gmail.com>
2022-10-16 20:12:39 +0300

    edited 2

--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,12 +1,13 @@
-import { PortfolioItem } from '../components/PortfolioItem';
-import { items } from '../data/Items';
+// import { PortfolioItem } from '../components/PortfolioItem';
+// import { items } from '../data/Items';
 
 export default function PortfolioPage() {
 
   
   return (
     <div className="Portfolio-content">
-      {items.map(item => <PortfolioItem item={item} key={item.id}/>)}
+      <p>This page will be updated...</p>
+      {/* {items.map(item => <PortfolioItem item={item} key={item.id}/>)} */}
     </div>
   );
 }
