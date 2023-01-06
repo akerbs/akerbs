f5631a62f0d031ab1d1f4d1c0211b50cc9d7f114
Anatoliy Kerbs <akerbs@bellintegrator>
2023-01-06 11:27:22 +0300

    links updated

--- src/data/Items.ts
+++ src/data/Items.ts
@@ -1,32 +1,37 @@
-import { IItem } from '../models/models'
-
+import { IItem } from "../models/models";
 
 export const items: IItem[] = [
   {
     id: 8,
-    name: 'Ecommerce',
+    name: "Ecommerce",
     year: 2020,
-    img: 'item8.jpg',
-    stack: 'HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS, Google Analytics',
-    description: 'Create responsive multilingual and multicurrency e-store using Stripe API.',
-    url: 'https://ecommerce-akerbs.vercel.app/',
+    img: "item8.jpg",
+    stack:
+      "HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS, Google Analytics",
+    description:
+      "Create responsive multilingual and multicurrency e-store using Stripe API.",
+    url: "https://ecommerce-akerbs.netlify.app/",
   },
   {
     id: 7,
-    name: 'Service Bureau',
+    name: "Service Bureau",
     year: 2020,
-    img: 'item7.jpg',
-    stack: 'HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS, Google Analytics',
-    description: 'Create a responsive multilingual website of international medical, legal and personnel services.',
-    url: 'https://salvegeorgia-demo.vercel.app/',
+    img: "item7.jpg",
+    stack:
+      "HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS, Google Analytics",
+    description:
+      "Create a responsive multilingual website of international medical, legal and personnel services.",
+    url: "https://salvegeorgia-demo.netlify.app/",
   },
   {
     id: 6,
-    name: 'Restaurant',
+    name: "Restaurant",
     year: 2020,
-    img: 'item6.jpg',
-    stack: 'HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS, Google Analytics',
-    description: 'Create a responsive multilingual restaurant website with table reservations.',
-    url: 'https://suliko-hamburg.vercel.app/',
+    img: "item6.jpg",
+    stack:
+      "HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS, Google Analytics",
+    description:
+      "Create a responsive multilingual restaurant website with table reservations.",
+    url: "https://suliko-hamburg.netlify.app/",
   },
-]
+];
